e24cb1ec6e21eea4315f394b5ed1985e86b2669c
Franco Ezequiel <eze.cavs@hotmail.com>
2022-01-06 20:50:06 -0300

    Estructura creada del header, es complejo y detallado, va a llevar tiempo este proyecto

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './Header.css';
 import { Navbar, Container, Nav, NavDropdown } from 'react-bootstrap';
-import Logo from '../../Images/logo.png'
+import Logo from '../../Images/logo.png';
+import DisneyPlus from '../../Images/headerDisneyPlus.png';
 function Header() {
   return (
       <header className="header">
-      <Navbar bg="light" expand="lg">
+      <Navbar className='navBarContainer' expand="lg">
         <Container>
             <Navbar.Brand> <img src={Logo} alt="" /></Navbar.Brand>
             <input type="text" />
@@ -19,8 +20,17 @@ function Header() {
                 <Nav.Link href="#link">Register</Nav.Link>
             </Nav>
             </Navbar.Collapse>
+            <img src={DisneyPlus} alt="" />
         </Container>
     </Navbar>
+    <div className='navBarDeskTop'>
+        <ul>
+            <li>123</li>
+            <li>123</li>
+            <li>123</li>
+            <li>1234</li>
+        </ul>
+    </div>
       </header>
   );
 }
